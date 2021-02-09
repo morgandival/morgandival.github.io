01b93f2229c7f91fd17fb07d07665fedaf3646be
Morgan Di Val <morgandival@gmail.com>
2021-02-09 13:32:10 +1100

    Refactored JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 window.onload = function() {
   var date = new Date();
-  console.log("Date = " + date);
-
-  var year = date.getFullYear();
-  console.log(year);
-
-  document.getElementById("copyyear").innerText = year;
+  document.getElementById("copyyear").innerText = date.getFullYear();
 }
