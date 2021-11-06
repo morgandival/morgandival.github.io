cc32ee5a03e913619403c3f699dcbc5cef0f6475
Morgan Di Val <morgandival@gmail.com>
2021-11-06 13:25:38 +1100

    Cleared files for new build

--- ts/copyyear.ts
+++
@@ -1,6 +0,0 @@
-// dynamically generate current year on pageload and display on page
-window.onload = function () {
-  document.getElementById(
-    'copyyear',
-  ).innerText = new Date().getFullYear().toString();
-};
