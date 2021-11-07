ad6993ef6b86eae8f7aef8249b7ecc4e6c56e0aa
Morgan Di Val <morgandival@gmail.com>
2021-11-08 01:44:37 +1100

    Added height to header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="grid grid-cols-12 mb-4 bg-indigo-dye text-white">
+    <header className="grid grid-cols-12 h-24 mb-4 bg-indigo-dye text-white">
       <div className="col-start-3 col-end-7 py-5 self-center text-4xl font-bold">
         <Link href="/">
           <a>
