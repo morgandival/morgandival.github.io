a3d0cf1feb340edf37631d61f308821966823790
Morgan Di Val <morgandival@gmail.com>
2021-11-09 12:21:09 +1100

    Tinkered with responsive styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header>
-      <div className="col-start-3 col-end-7 text-4xl font-bold">
-        <h1>
+      <div className="col-span-full sm:col-start-2 sm:col-end-7 lg:col-start-3 text-center sm:text-left text-4xl font-bold">
+        <h1 className="my-4">
           <Link href="/">
             <a>Morgan Di Val</a>
           </Link>
         </h1>
       </div>
-      <div className="col-start-7 col-end-11 text-xl text-right">
+      <div className="col-span-full sm:col-start-7 sm:col-end-12 lg:col-end-11 text-xl text-center sm:text-right">
         <ul>
           <li className="inline-block m-3">
             <Link href="/about">
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,17 @@ import heroImage from '../public/Morgan.jpg';
 
 const Home: NextPage = () => {
   return (
-    <div className="grid grid-cols-12 items-center my-8">
+    <div className="grid grid-cols-12 items-center my-8 gap-y-4">
       <Head>
         <title>Morgan Di Val - Home</title>
       </Head>
-      <div className="col-start-3 col-end-8">
+      <div className="col-start-2 col-end-12 sm:col-start-2 sm:col-end-8 lg:col-start-3 md:text-left">
         <h2 className="text-2xl mr-4">
           G&apos;day! I&apos;m Morgan, a web developer and content writer based
           in Sydney Australia.
         </h2>
       </div>
-      <div className="col-start-8 col-end-11 text-right">
+      <div className="col-start-2 col-end-12 sm:col-start-8 sm:col-end-11 md:col-end-12 lg:col-end-11 text-right">
         <Image src={heroImage} alt="Morgan" className="rounded-full ml-4" />
       </div>
     </div>
