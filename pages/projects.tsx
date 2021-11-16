2fd30245ec7dd24f6797aee1cad1b33bbf55e4d0
Morgan Di Val <morgandival@gmail.com>
2021-11-16 16:18:46 +1100

    Added styling to main div, added props to project card

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,11 +4,15 @@ import ProjectCard from '../components/ProjectCard';
 
 const Projects: NextPage = () => {
   return (
-    <div>
+    <div className="grid col-span-full grid-cols-12 my-4">
       <Head>
         <title>Morgan Di Val - Projects</title>
       </Head>
-      <ProjectCard title="FooBar" description={['alpha', 'beta']} image="" />
+      <ProjectCard
+        title="FooBar"
+        description={['alpha', 'beta']}
+        image="placeholder"
+      />
     </div>
   );
 };
