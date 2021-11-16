8fd8770c12171285cf67a1d7e6f2b4e2086f3087
Morgan Di Val <morgandival@gmail.com>
2021-11-16 16:20:18 +1100

    Added styles to project card component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 type Props = {
   title: string;
   description: Array<string>;
@@ -6,7 +8,7 @@ type Props = {
 
 const ProjectCard = (props: Props) => {
   return (
-    <div>
+    <div className="col-start-2 col-end-12 lg:col-start-3 lg:col-end-11 my-4 p-4 bg-light-gray rounded">
       <p>{props.title}</p>
       <p>{props.description}</p>
       <p>{props.image}.jpg</p>
