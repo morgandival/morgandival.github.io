a773ba11939c7c9e69de333fbda8fb1f521839c9
Morgan Di Val <morgandival@gmail.com>
2021-11-17 18:46:30 +1100

    Modified project card styles

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const ProjectCard = (props: Props) => {
-  let output: Array<JSX.Element> = [];
+  const output: Array<JSX.Element> = [];
 
   props.description.forEach((value) => {
     output.push(<p>{value}</p>);
@@ -16,8 +16,8 @@ const ProjectCard = (props: Props) => {
   const projectCardImage = `/${props.image}.jpg`;
 
   return (
-    <div className="col-start-2 col-end-12 lg:col-start-3 lg:col-end-11 my-4 p-4 bg-light-gray rounded-lg text-center flex">
-      <div>
+    <div className="col-start-2 col-end-12 lg:col-start-3 lg:col-end-11 my-4 p-4 bg-light-gray rounded-lg text-center flex flex-grow flex-wrap">
+      <div className="flex-grow">
         <h3 className="text-xl font-bold">{props.title}</h3>
         {output}
         <p>Insert image here.</p>
