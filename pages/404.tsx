5f7c2defdb8f698f1a03668bf2d4f27c68a1aa45
Morgan Di Val <morgandival@gmail.com>
2021-11-21 14:10:17 +1100

    Added non-default head, centered text on page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const NotFound: NextPage = () => {
-  return <div>This is not the web page you are looking for.</div>;
+  return (
+    <div className="flex flex-col h-full">
+      <Head>
+        <title>Morgan Di Val - Page not found</title>
+      </Head>
+      <div className="m-auto">
+        <p>This is not the page you are looking for</p>
+        <p>Back to home</p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
