fed6a06c1fa3d380d6870badf4d55d76eaff1d1a
Morgan Di Val <morgandival@gmail.com>
2021-11-21 14:37:41 +1100

    Added new project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,12 +11,23 @@ const Projects: NextPage = () => {
       <ProjectCard
         title="Till Counter"
         description={[
-          'This project is a basic React App that lets you count all of the money by adding a series of input fields together and displaying a total.',
+          'This project is a basic React app that lets you count all of the money by adding a series of input fields together and displaying a total.',
           'Tech used: React, TypeScript',
         ]}
         image="/till-counter.jpg"
         liveLink="https://morgandival.github.io/till-counter"
         repoLink="https://github.com/morgandival/till-counter"
+      />
+      <ProjectCard
+        title="Dice Roller"
+        description={[
+          'A basic React app that generates random dice rolls based on the selected die.',
+          'Dice Available: d4, d6, d8, d10, d12, d20, d100.',
+          'Tech used: React, TypeScript.',
+        ]}
+        image="/dice-roller.jpg"
+        liveLink="https://morgandival.github.io/dice-roller"
+        repoLink="https://github.com/morgandival/dice-roller"
       />
     </div>
   );
