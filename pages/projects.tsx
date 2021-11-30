a7d27046f7b1672ccbe463f090a2e92f8c1c1148
Morgan Di Val <morgandival@gmail.com>
2021-12-01 02:29:34 +1100

    Created array of card objects and looped through them instead of calling individually

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,32 +3,44 @@ import Head from 'next/head';
 import ProjectCard from '../components/ProjectCard';
 
 const Projects: NextPage = () => {
+  const projectCards = [
+    {
+      title: 'Till Counter',
+      description: [
+        'This project is a basic React app that lets you count all of the money by adding a series of input fields together and displaying a total.',
+        'Tech used: React, TypeScript',
+      ],
+      slug: 'till-counter',
+    },
+    {
+      title: 'Dice Roller',
+      description: [
+        'A basic React app that generates random dice rolls based on the selected die.',
+        'Dice Available: d4, d6, d8, d10, d12, d20, d100.',
+        'Tech used: React, TypeScript.',
+      ],
+      slug: 'dice-roller',
+    },
+  ];
+
+  const cardOutput: Array<JSX.Element> = [];
+
+  projectCards.forEach((card) => {
+    cardOutput.push(
+      <ProjectCard
+        title={card.title}
+        description={card.description}
+        slug={card.slug}
+      />
+    );
+  });
+
   return (
     <div className="grid col-span-full grid-cols-12 my-4">
       <Head>
         <title>Morgan Di Val - Projects</title>
       </Head>
-      <ProjectCard
-        title="Till Counter"
-        description={[
-          'This project is a basic React app that lets you count all of the money by adding a series of input fields together and displaying a total.',
-          'Tech used: React, TypeScript',
-        ]}
-        image="/till-counter.jpg"
-        liveLink="https://morgandival.github.io/till-counter"
-        repoLink="https://github.com/morgandival/till-counter"
-      />
-      <ProjectCard
-        title="Dice Roller"
-        description={[
-          'A basic React app that generates random dice rolls based on the selected die.',
-          'Dice Available: d4, d6, d8, d10, d12, d20, d100.',
-          'Tech used: React, TypeScript.',
-        ]}
-        image="/dice-roller.jpg"
-        liveLink="https://morgandival.github.io/dice-roller"
-        repoLink="https://github.com/morgandival/dice-roller"
-      />
+      {cardOutput}
     </div>
   );
 };
