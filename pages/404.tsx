be63703be66e44a99d868b8fb45d6c6b25d8e925
Morgan Di Val <morgandival@gmail.com>
2021-12-07 03:50:27 +1100

    Modified style classes

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 const NotFound: NextPage = () => {
   return (
-    <div className="flex flex-col h-full">
+    <div className="content">
       <Head>
         <title>Morgan Di Val - Page not found</title>
       </Head>
-      <div className="m-auto text-2xl">
-        <p className="m-4">This is not the page you are looking for.</p>
-        <p className="m-4">
+      <div className="col-start-2 col-end-12 lg:col-start-3 lg:col-end-11 text-2xl">
+        <p className="my-8">This is not the page you are looking for.</p>
+        <p className="my-8">
           <Link href="/">
             <a className="bg-raisin-black text-white rounded-lg py-2 px-6">
               Back to home
