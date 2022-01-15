1b9c324a6975832635839897e84370eb6aa7a948
Morgan Di Val <morgandival@gmail.com>
2022-01-15 22:41:46 +1100

    Modified image import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import heroImage from '../public/morgan.jpg';
+import heroImage from '/public/morgan.jpg';
 
 const Index: NextPage = () => {
   return (
