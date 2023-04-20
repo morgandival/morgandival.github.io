f246d6144de4700e0635f4792ed8943e73341c0f
Morgan Di Val <morgandival@gmail.com>
2023-04-20 22:15:30 +1000

    Made copyright year dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+const year = new Date().getFullYear();
+
 const Footer = (): JSX.Element => {
   return (
     <footer>
       <div className="col-start-3 col-end-11 text-center text-xl">
-        <p className="p-6">Morgan Di Val &copy; 2021</p>
+        <p className="p-6">Morgan Di Val &copy; {year}</p>
       </div>
     </footer>
   );
